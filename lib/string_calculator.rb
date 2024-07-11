768242df8cacd9bef423127bcd7ee89f26ca622d
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-11 19:34:16 +0530

    Refactored codebase, switched from class methods to public/private methods

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,56 +4,56 @@ require 'byebug'
 class StringCalculator
   DELIMITER_LIST = /,|\n/.freeze
 
-  class << self
-    def add(numbers_list)
-      # Nil Case "", nil
-      return 0 if numbers_list.empty? || numbers_list.nil?
+  def add(numbers_list)
+    # Nil Case "", nil
+    return 0 if numbers_list.empty? || numbers_list.nil?
 
-      # Case "1", returns 1
-      if numbers_list.split(DELIMITER_LIST).count < 2 && !numbers_list.start_with?('//') && !numbers_list.to_i.negative?
-        return numbers_list.to_i
-      end
-
-      # Add Input: “1,5”, Output: 6
-      if numbers_list.start_with?('//')
-        custom_delimiter, numbers_list = parse_custom_delimiter_with_numbers_list(numbers_list)
-      end
-      custom_delimiter ||= ''
-
-      delimiter_list = add_delimiter_to_existing_delimiter_list(custom_delimiter)
-
-      numbers = numbers_list.split(delimiter_list).map(&:to_i)
-
-      # Handle case for negative numbers
-      handle_negative_numbers(numbers)
-
-      numbers.reduce(0, :+)
+    # Case "1", returns 1
+    if numbers_list.split(DELIMITER_LIST).count < 2 && !numbers_list.start_with?('//') && !numbers_list.to_i.negative?
+      return numbers_list.to_i
     end
 
-    def parse_custom_delimiter_with_numbers_list(input)
-      custom_delimiter, numbers_list = input.match(%r{//(.+)\n((.|\n)*)}).captures
-
-      [custom_delimiter, numbers_list]
+    # Add Input: “1,5”, Output: 6
+    if numbers_list.start_with?('//')
+      custom_delimiter, numbers_list = parse_custom_delimiter_with_numbers_list(numbers_list)
     end
+    custom_delimiter ||= ''
 
-    def add_delimiter_to_existing_delimiter_list(new_delimiter)
-      return DELIMITER_LIST if new_delimiter.empty?
+    delimiter_list = add_delimiter_to_existing_delimiter_list(custom_delimiter)
 
-      # Escape the new delimiter to handle special characters
-      escaped_delimiter = Regexp.escape(new_delimiter)
+    numbers = numbers_list.split(delimiter_list).map(&:to_i)
 
-      # Append the new delimiter to the base pattern
-      modified_pattern = "#{DELIMITER_LIST.source}|#{escaped_delimiter}"
+    # Handle case for negative numbers
+    handle_negative_numbers(numbers)
 
-      Regexp.new(modified_pattern)
-    end
+    numbers.reduce(0, :+)
+  end
 
-    def handle_negative_numbers(numbers)
-      negative_numbers = numbers.select(&:negative?)
+  private
 
-      return if negative_numbers.empty?
+  def parse_custom_delimiter_with_numbers_list(input)
+    custom_delimiter, numbers_list = input.match(%r{//(.+)\n((.|\n)*)}).captures
 
-      raise "Negative numbers not allowed: #{negative_numbers.join(', ')}"
-    end
+    [custom_delimiter, numbers_list]
+  end
+
+  def add_delimiter_to_existing_delimiter_list(new_delimiter)
+    return DELIMITER_LIST if new_delimiter.empty?
+
+    # Escape the new delimiter to handle special characters
+    escaped_delimiter = Regexp.escape(new_delimiter)
+
+    # Append the new delimiter to the base pattern
+    modified_pattern = "#{DELIMITER_LIST.source}|#{escaped_delimiter}"
+
+    Regexp.new(modified_pattern)
+  end
+
+  def handle_negative_numbers(numbers)
+    negative_numbers = numbers.select(&:negative?)
+
+    return if negative_numbers.empty?
+
+    raise "Negative numbers not allowed: #{negative_numbers.join(', ')}"
   end
 end
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -4,63 +4,67 @@ require 'minitest/autorun'
 require_relative '../lib/string_calculator'
 
 class StringCalculatorTest < Minitest::Test
+  def setup
+    @string_calculator = StringCalculator.new
+  end
+
   def test_add_empty_string
-    assert_equal 0, StringCalculator.add('')
+    assert_equal 0, @string_calculator.add('')
   end
 
   def test_add_single_number
-    assert_equal 1, StringCalculator.add('1')
+    assert_equal 1, @string_calculator.add('1')
   end
 
   def test_add_single_number_with_newline_or_comma_delimiters
-    assert_equal 1, StringCalculator.add('1')
-    assert_equal 1, StringCalculator.add("1\n")
-    assert_equal 1, StringCalculator.add('1,')
+    assert_equal 1, @string_calculator.add('1')
+    assert_equal 1, @string_calculator.add("1\n")
+    assert_equal 1, @string_calculator.add('1,')
   end
 
   def test_add_single_negative_number_with_newline_or_comma_delimiters_raises_exception
-    exception = assert_raises(RuntimeError) { StringCalculator.add('-1') }
+    exception = assert_raises(RuntimeError) { @string_calculator.add('-1') }
     assert_equal 'Negative numbers not allowed: -1', exception.message
 
-    exception = assert_raises(RuntimeError) { StringCalculator.add('-1,') }
+    exception = assert_raises(RuntimeError) { @string_calculator.add('-1,') }
     assert_equal 'Negative numbers not allowed: -1', exception.message
   end
 
   def test_add_single_number_with_custom_delimiter
-    assert_equal 1, StringCalculator.add("//;\n1")
-    assert_equal 1, StringCalculator.add("//%\n1%")
-    assert_equal 1, StringCalculator.add("//%\n%%%1%%%")
+    assert_equal 1, @string_calculator.add("//;\n1")
+    assert_equal 1, @string_calculator.add("//%\n1%")
+    assert_equal 1, @string_calculator.add("//%\n%%%1%%%")
   end
 
   def test_add_two_numbers
-    assert_equal 6, StringCalculator.add('1, 5')
+    assert_equal 6, @string_calculator.add('1, 5')
   end
 
   def test_add_two_numbers_with_newline
-    assert_equal 6, StringCalculator.add("1\n5")
+    assert_equal 6, @string_calculator.add("1\n5")
   end
 
   def test_add_list_of_multiple_numbers
-    assert_equal 160, StringCalculator.add('1, 5, 4, 10, 20, 30, 40, 50')
+    assert_equal 160, @string_calculator.add('1, 5, 4, 10, 20, 30, 40, 50')
   end
 
   def test_add_list_of_multiple_numbers_with_newline_comma_delimiters
-    assert_equal 160, StringCalculator.add('1, 5, 4, 10, 20, 30, 40, 50')
-    assert_equal 160, StringCalculator.add("1\n 5\n 4\n 10\n 20\n 30\n 40\n 50")
-    assert_equal 160, StringCalculator.add("1, 5\n 4, 10\n 20, 30\n 40, 50")
-    assert_equal 160, StringCalculator.add("1\n 5, 4\n 10\n 20, 30\n 40, 50")
+    assert_equal 160, @string_calculator.add('1, 5, 4, 10, 20, 30, 40, 50')
+    assert_equal 160, @string_calculator.add("1\n 5\n 4\n 10\n 20\n 30\n 40\n 50")
+    assert_equal 160, @string_calculator.add("1, 5\n 4, 10\n 20, 30\n 40, 50")
+    assert_equal 160, @string_calculator.add("1\n 5, 4\n 10\n 20, 30\n 40, 50")
   end
 
   def test_add_list_of_multiple_numbers_with_custom_newline_and_comma_delimiters
-    assert_equal 160, StringCalculator.add("//$\n1, 5$ 4, 10, 20$ 30, 40, 50")
-    assert_equal 160, StringCalculator.add("//$\n1\n 5$ 4\n 10, 20$ 30\n 40$ 50")
+    assert_equal 160, @string_calculator.add("//$\n1, 5$ 4, 10, 20$ 30, 40, 50")
+    assert_equal 160, @string_calculator.add("//$\n1\n 5$ 4\n 10, 20$ 30\n 40$ 50")
   end
 
   def test_add_list_of_multiple_numbers_including_negatives_with_all_delimiters_combinations_raising_exception
-    exception = assert_raises(RuntimeError) { StringCalculator.add("//$\n1, 5$ -4, 10, 20$ 30, 40, -50") }
+    exception = assert_raises(RuntimeError) { @string_calculator.add("//$\n1, 5$ -4, 10, 20$ 30, 40, -50") }
     assert_equal 'Negative numbers not allowed: -4, -50', exception.message
 
-    exception = assert_raises(RuntimeError) { StringCalculator.add("//$\n1\n -5$ 4\n 10, -20$ 30\n -40$ 50") }
+    exception = assert_raises(RuntimeError) { @string_calculator.add("//$\n1\n -5$ 4\n 10, -20$ 30\n -40$ 50") }
     assert_equal 'Negative numbers not allowed: -5, -20, -40', exception.message
   end
 end
