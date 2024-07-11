a98f27521f8c23a815a64f74513dc4763cb47d4b
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-11 16:13:14 +0530

    [3] - Allow the add method to handle new lines between numbers     - Refactored logic to handle both comman and newline delimiters     - Added unit test for comman and newline delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,13 +1,15 @@
 class StringCalculator
+  DELIMITER_LIST = /,|\n/
+
   def self.add(numbers_list)
     # Nil Case "", nil
     return 0 if numbers_list.empty? || numbers_list.nil?
 
     # Case "1", returns 1
-    return numbers_list.to_i unless numbers_list.include? ","
+    return numbers_list.to_i if numbers_list.split(DELIMITER_LIST).count < 2
 
     # Add Input: “1,5”, Output: 6
-    numbers = numbers_list.split(",").map(&:to_i)
+    numbers = numbers_list.split(DELIMITER_LIST).map(&:to_i)
     numbers.reduce(0, :+)
   end  
 end
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -10,11 +10,28 @@ class StringCalculatorTest < Minitest::Test
     assert_equal 1, StringCalculator.add("1")
   end
 
+  def test_add_single_number_with_newline_or_comma_delimiters
+    assert_equal 1, StringCalculator.add("1")
+    assert_equal 1, StringCalculator.add("1\n")
+    assert_equal 1, StringCalculator.add("1,")
+  end
+
   def test_add_two_numbers
     assert_equal 6, StringCalculator.add("1, 5")
   end
 
+  def test_add_two_numbers_with_newline
+    assert_equal 6, StringCalculator.add("1\n5")
+  end
+
   def test_add_list_of_multiple_numbers
     assert_equal 160, StringCalculator.add("1, 5, 4, 10, 20, 30, 40, 50")
+  end
+
+  def test_add_list_of_multiple_numbers_with_newline_comma_delimiters
+    assert_equal 160, StringCalculator.add("1, 5, 4, 10, 20, 30, 40, 50")
+    assert_equal 160, StringCalculator.add("1\n 5\n 4\n 10\n 20\n 30\n 40\n 50")
+    assert_equal 160, StringCalculator.add("1, 5\n 4, 10\n 20, 30\n 40, 50")
+    assert_equal 160, StringCalculator.add("1\n 5, 4\n 10\n 20, 30\n 40, 50")
   end
 end
